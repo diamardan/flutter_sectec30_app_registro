d09743ef351d4962e15915b47565aa7bfac4dd41
Daniel Díaz Martínez <diamardan@gmail.com>
2021-08-28 13:03:05 -0500

    cambios actuales en tienda de ios

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,17 @@ import 'package:cetis32_app_registro/src/wrapper.dart';
 import 'package:cetis32_app_registro/src/bloc/deep_link_bloc.dart';
 
 void main() async {
-   WidgetsFlutterBinding.ensureInitialized();
-   await Firebase.initializeApp();
-  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   Future.delayed(const Duration(seconds: 3), () {
     print("10 segundos");
     return runApp(MyApp());
   });
-   
 }
 
 class MyApp extends StatelessWidget {
-   DeepLinkBloc _bloc = DeepLinkBloc();
+  DeepLinkBloc _bloc = DeepLinkBloc();
   @override
   Widget build(BuildContext context) {
 /*DeepLinkBloc _bloc = DeepLinkBloc();
@@ -25,17 +24,12 @@ class MyApp extends StatelessWidget {
                 create: (context) => _bloc,
                 dispose: (context, bloc) => bloc.dispose(),
                 child:*/
-                return MaterialApp(
-      title: 'CETIS 32 APP REGISTRO',
-      debugShowCheckedModeBanner: false,
-      routes: getApplicationRoutes(),
-      theme: ThemeData(
-        primaryColor: AppColors.morenaColor
-      ),
-     // initialRoute: 'inicio',
-      home: Wrapper() /* Scaffold(body: Center(child:Text("Hola")) )*/   );
-    
-    
+    return MaterialApp(
+        title: 'CETIS 32 APP REGISTRO',
+        debugShowCheckedModeBanner: false,
+        routes: getApplicationRoutes(),
+        theme: ThemeData(primaryColor: AppColors.morenaColor),
+        // initialRoute: 'inicio',
+        home: Wrapper() /* Scaffold(body: Center(child:Text("Hola")) )*/);
   }
-  
 }
