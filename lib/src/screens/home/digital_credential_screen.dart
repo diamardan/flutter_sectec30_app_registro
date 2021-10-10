054a4b4bd02b1c0eababa69b68a6116a8cdd813a
Daniel Díaz Martínez <diamardan@gmail.com>
2021-10-10 18:12:19 -0500

    pdf generado y con snackbars

--- lib/src/screens/home/digital_credential_screen.dart
+++ lib/src/screens/home/digital_credential_screen.dart
@@ -1,12 +1,14 @@
 import 'dart:typed_data';
-
+import 'dart:io';
 import 'package:cetis32_app_registro/src/constants/constants.dart';
 import 'package:cetis32_app_registro/src/models/user_model.dart';
 import 'package:cetis32_app_registro/src/utils/imageUtil.dart';
 import 'package:cetis32_app_registro/src/utils/widget_to_image.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:permission_handler/permission_handler.dart';
 
 class DigitalCredentialScreen extends StatefulWidget {
   final Register register;
@@ -34,6 +36,7 @@ class _DigitalCredentialScreenState extends State<DigitalCredentialScreen> {
           backgroundColor: AppColors.morenaColor,
           child: Icon(Icons.download),
           onPressed: () async {
+            _showSnackbar("Su descarga comenzará en breve");
             final bytes1 = await ImageUtils.capture(key1);
             final bytes2 = await ImageUtils.capture(key2);
 
@@ -41,56 +44,13 @@ class _DigitalCredentialScreenState extends State<DigitalCredentialScreen> {
               this.bytes1 = bytes1;
               this.bytes2 = bytes2;
             });
+            makePdf();
           }),
       body: SafeArea(
           child: SingleChildScrollView(
         child: Center(
           child: Column(children: <Widget>[
-            WidgetToImage(builder: (key) {
-              this.key1 = key;
-              return Card(
-                /* semanticContainer: true,
-                clipBehavior: Clip.antiAliasWithSaveLayer, */
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                elevation: 10,
-                child: Container(
-                  height: 490,
-                  width: 310,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        alignment: Alignment.topCenter,
-                        image: AssetImage('assets/img/credencial/anverso.png')),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 128),
-                      _cintillaFoto(),
-                      _cintillaBlanca(),
-                      _cintillaNombre(),
-                      _cintillaEspecialidad(),
-                      _cintillaNumeroControl(),
-                      /* Container(
-                        width: double.infinity,
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              //color: AppColors.morenaColor,
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10))),
-                          width: double.infinity,
-                          child: Text("hola 1"),
-                        ), 
-                      )*/
-                    ],
-                  ),
-                ),
-              );
-            }),
+            _anversoCredencial(),
             WidgetToImage(builder: (key) {
               this.key2 = key;
               return Card(
@@ -119,12 +79,60 @@ class _DigitalCredentialScreenState extends State<DigitalCredentialScreen> {
                 ),
               );
             }),
-            buildImage(bytes1),
-            buildImage(bytes2),
+            /* buildImage(bytes1),
+            buildImage(bytes2), */
           ]),
         ),
       )),
     );
+  }
+
+  Widget _anversoCredencial() {
+    return WidgetToImage(builder: (key) {
+      this.key1 = key;
+      return Card(
+        /* semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer, */
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation: 10,
+        child: Container(
+          height: 490,
+          width: 310,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+                image: AssetImage('assets/img/credencial/anverso.png')),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 128),
+              _cintillaFoto(),
+              _cintillaBlanca(),
+              _cintillaNombre(),
+              _cintillaEspecialidad(),
+              _cintillaNumeroControl(),
+              /* Container(
+                        width: double.infinity,
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              //color: AppColors.morenaColor,
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10))),
+                          width: double.infinity,
+                          child: Text("hola 1"),
+                        ), 
+                      )*/
+            ],
+          ),
+        ),
+      );
+    });
   }
 
   Widget _cintillaFoto() {
@@ -437,6 +445,62 @@ class _DigitalCredentialScreenState extends State<DigitalCredentialScreen> {
 
   Widget buildImage(Uint8List bytes) =>
       bytes != null ? Image.memory(bytes) : Container();
+
+  void _showSnackbar(String content) {
+    final snackBar = SnackBar(content: (Text(content)));
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<void> makePdf() async {
+    final pdf = pw.Document();
+
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Row(
+            mainAxisAlignment: pw.MainAxisAlignment.start,
+            children: <pw.Widget>[
+              pw.Image(
+                pw.MemoryImage(
+                  bytes1,
+                ),
+                height: 300,
+                //fit: pw.BoxFit.fitHeight
+              ),
+              pw.SizedBox(width: 10),
+              pw.Image(
+                pw.MemoryImage(
+                  bytes2,
+                ),
+                height: 300,
+                //fit: pw.BoxFit.fitHeight
+              ),
+              // ImageImage(bytes1),
+              //pw.Image(pw.MemoryImage(bytes1)),
+            ]),
+      ),
+    );
+
+// the downloads folder path
+    //Directory output = await getDownloadsDirectory();
+    String tempPath = '/storage/emulated/0/Download';
+    //output.path;
+    var filePath = tempPath + '/${widget.register.curp}.pdf';
+    final file = File(filePath);
+    //
+    /* final output = await getExternalStorageDirectory();
+    final path = "${output.path}/credencial.pdf";
+    final file = File(path); */
+    print(filePath);
+    /* final file = File('example.pdf');*/
+    await file.writeAsBytes(await pdf.save());
+    print("hola");
+    _showSnackbar("El PDF está en su carpeta de Descargas");
+  }
 }
 
 /* class DigitalCredentialScreen extends StatelessWidget {
